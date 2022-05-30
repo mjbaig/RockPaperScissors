0518b5df137cd9529aa01308d6e175326dc3a9d1
Maz Baig <7367135+mjbaig@users.noreply.github.com>
2022-05-30 03:36:07 -0500

    changed some methods to make the client communication work

--- web-client/app.js
+++ web-client/app.js
@@ -7,4 +7,14 @@ var connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:7
 
 connection.start().then(e => console.log(`started ${e}`)).catch(e => console.error(e));
 
-connection.invoke("SignIn", "neebu").then(e => console.log(`hi ${e}`)).catch(e => console.log(e));
+function signIn(name) {
+    connection.invoke("signIn", name).then(e => console.log(`hi ${e}`)).catch(e => console.log(e));
+}
+
+function joinQueue(name) {
+    connection.invoke("joinQueue", name).then(e => console.log(`hi ${e}`)).catch(e => console.log(e));
+}
+
+connection.on("getState", (e) => console.log(e));
+
+console.log("loaded");
